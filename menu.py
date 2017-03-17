871394888be867486893f2a8f37ff6859e108423
RedPandaDev <katukasz@gmail.com>
2017-03-17 18:38:26 +0000

    Added basic menu file
    Basic python menu. No buttons yet.  File menu added and open file there.

---
+++ menu.py
@@ -0,0 +1,55 @@
+from tkinter import *
+from tkinter.filedialog import askopenfilename as aofn
+
+
+def donothing():
+    filewin = Toplevel(root)
+    button = Button(filewin, text="Do nothing button")
+    button.pack()
+
+
+def OpenFile():
+    name = aofn(initialdir="C:/Users/Batman/Documents/Programming/tkinter/",
+                filetypes=(("Text File", "*.txt"), ("All Files", "*.*")),
+                title="Choose a file.")
+    print(name)
+
+    try:
+        with open(name, 'r') as UseFile:
+            print(UseFile.read())
+    except:
+        print("No file exists")
+
+
+root = Tk()
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="New", command=donothing)
+filemenu.add_command(label="Open", command=OpenFile)
+filemenu.add_command(label="Save", command=donothing)
+filemenu.add_command(label="Save as...", command=donothing)
+filemenu.add_command(label="Close", command=donothing)
+
+filemenu.add_separator()
+
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+editmenu = Menu(menubar, tearoff=0)
+editmenu.add_command(label="Undo", command=donothing)
+
+editmenu.add_separator()
+
+editmenu.add_command(label="Cut", command=donothing)
+editmenu.add_command(label="Copy", command=donothing)
+editmenu.add_command(label="Paste", command=donothing)
+editmenu.add_command(label="Delete", command=donothing)
+editmenu.add_command(label="Select All", command=donothing)
+
+menubar.add_cascade(label="Edit", menu=editmenu)
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="Help Index", command=donothing)
+helpmenu.add_command(label="About...", command=donothing)
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+root.config(menu=menubar)
+root.mainloop()
